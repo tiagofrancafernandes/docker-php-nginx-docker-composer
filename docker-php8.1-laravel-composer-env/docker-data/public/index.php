e01b05e752cbfb0c24b9d1325e60572dee739249
Tiago França <tiago@tiagofranca.com>
2022-05-26 10:54:51 -0300

    add php8.1 environment
    No ramo docker-php8.1-laravel-composer-env
    Mudanças a serem submetidas:
    	new file:   docker-php8.1-laravel-composer-env/.gitignore
    	new file:   docker-php8.1-laravel-composer-env/docker-compose.yml
    	new file:   docker-php8.1-laravel-composer-env/docker-data/Dockerfile
    	new file:   docker-php8.1-laravel-composer-env/docker-data/nginx/conf.d/app.conf
    	new file:   docker-php8.1-laravel-composer-env/docker-data/php/local.ini
    	new file:   docker-php8.1-laravel-composer-env/docker-data/profile-files/bash_alises.sh
    	new file:   docker-php8.1-laravel-composer-env/docker-data/profile-files/bash_history.txt
    	new file:   docker-php8.1-laravel-composer-env/docker-data/profile-files/psysh/psysh_history
    	new file:   docker-php8.1-laravel-composer-env/docker-data/public/index.php
    	new file:   docker-php8.1-laravel-composer-env/docker-data/public/info.php
    	new file:   docker-php8.1-laravel-composer-env/docker-example.env

---
+++ docker-php8.1-laravel-composer-env/docker-data/public/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Demo page</title>
+</head>
+<body>
+    <div style="width: 50%;margin: 12rem auto;text-align: center;font-size: 1rem;border: 1px black solid; padding: 1rem;">
+        <a href="./info.php">
+            PHP Info
+        </a>
+    </div>
+</body>
+</html>
