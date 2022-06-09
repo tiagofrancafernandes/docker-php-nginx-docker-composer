714e31768dd7d95baa2a21f023ea2a8a2554fd1c
Tiago França <tiago@tiagofranca.com>
2022-06-08 23:38:45 -0300

    simple php8.1-fpm - local dev
    No ramo feat/pure-php81-fpm-dev
    Mudanças a serem submetidas:
    	new file:   .gitignore
    	new file:   Dockerfile
    	new file:   docker-compose.yml
    	new file:   docker-example.env
    	new file:   hello-world.php

---
+++ docker-php8.1-fpm-dev-server-env/hello-world.php
@@ -0,0 +1,49 @@
+<?php
+
+echo <<<"HTML"
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Hello World - by TF</title>
+</head>
+<body>
+    <h1>Hello World!</h1>
+    <h2>I am <a href="https://tiagofranca.com" target="_blank">Tiago França</a></h2>
+
+    <div class="row d-flex justify-content-center">
+        <div>
+            <a href="//www.linkedin.com/in/tiago-frança/" target="_blank" title="Linkedin">
+                Linkedin
+            </a>
+        </div>
+
+        <div>
+            <a href="//t.me/tiagofrancafernandes" title="Telegram">
+                Telegram
+            </a>
+        </div>
+
+        <div>
+            <a href="//github.com/tiagofrancafernandes" target="_blank" title="GitHub">
+                GitHub
+            </a>
+        </div>
+
+        <div>
+            <a href="//twitter.com/TiagoFOficial" target="_blank" title="Twitter">
+                Twitter
+            </a>
+        </div>
+
+        <div>
+            <a href="//www.facebook.com/Tiago.Franca.Fernandes" target="_blank" title="Facebook">
+                Facebook
+            </a>
+        </div>
+    </div>
+</body>
+</html>
+HTML;
